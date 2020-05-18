4038c8707665182450240e7461dc443e9350d9ac
daniel amezcua <dany_amezcua@live.com.mx>
2020-05-18 17:27:18 -0500

    add results with only reddit daily discussions

--- scripts/random_forests.py
+++ scripts/random_forests.py
@@ -4,50 +4,64 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+import numpy as np
 
-dataset_btc = pd.read_csv("./datasets/BTC.csv")
+dataset_btc = pd.read_csv("./datasets/ETH.csv")
+n = len(dataset_btc)
+x = []
+for i in range(0,n):
+	if dataset_btc.iloc[i]['close'] >= dataset_btc.iloc[i]['open']:
+		x.append(1)
+	else:
+		x.append(-1)
 
+dataset_btc['status'] = x
 
-#without considering the scores (17,18,19,20,21)
 dataset_btc['number_comments_ns'] = dataset_btc['neg_ns'] + dataset_btc['pos_ns'] + dataset_btc['neutral_ns']
 dataset_btc['ratio_pos_ns'] = dataset_btc['pos_ns']/dataset_btc['number_comments_ns']
 dataset_btc['ratio_neg_ns'] = dataset_btc['neg_ns']/dataset_btc['number_comments_ns']
 dataset_btc['ratio_neu_ns'] = dataset_btc['neutral_ns']/dataset_btc['number_comments_ns']
-dataset_btc['avg_compound_ns'] = dataset_btc['comp_ns']/dataset_btc['number_comments_ns']
+dataset_btc['avg_compound_ns'] = (dataset_btc['comp_ns']/dataset_btc['number_comments_ns'])
+dataset_btc['avg_pos_ns'] = (dataset_btc['sum_pos_ns']/dataset_btc['pos_ns']).replace((np.nan,np.inf, -np.inf), (0,0,0))
+dataset_btc['avg_neg_ns'] = (dataset_btc['sum_neg_ns']/dataset_btc['neg_ns']).replace((np.nan, np.inf, -np.inf), (0, 0, 0))
 
-# #x = dataset_btc.iloc[:, 17:21].values
-# print(dataset_btc["avg_compound_ns"])
-# #using just the number of comments
-# x = dataset_btc.iloc[:, [11,12,16,17,18,19,20]].values
-# y = dataset_btc.iloc[:, 15].values
-
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=10)
-# regressor = RandomForestClassifier(n_estimators=100, random_state=0)
-# regressor.fit(x_train, y_train)
-
-# y_pred = regressor.predict(x_test)
+x = dataset_btc.loc[:, ["open","close","high","low","avg_compound_ns"]].values
+y = dataset_btc.loc[:, "label"].values
 
 
-# print(confusion_matrix(y_test,y_pred))
-# print(classification_report(y_test,y_pred))
-# print(accuracy_score(y_test, y_pred))
+print("Without comment scores")
+accuracies = []
+for i in range(100):
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+	regressor = RandomForestClassifier(n_estimators=100)
+	regressor.fit(x_train, y_train)
 
-#considering the scores. 22,23,24,25,26,27
+	y_pred = regressor.predict(x_test)
 
-dataset_positive = dataset_btc[dataset_btc['label'] == 1]
-dataset_negative = dataset_btc[dataset_btc['label'] == -1]
-len_pos = len(dataset_positive)
-len_neg = len(dataset_negative)
+	accuracies.append(accuracy_score(y_test, y_pred))
 
-print("majority classifier: %f", len_pos/(len_pos+len_neg))
-print("majority classifier: %f", len_neg/(len_pos+len_neg))
+print("Accuracies mean:", np.mean(accuracies))
+print("Accuracies var:", np.var(accuracies))
+print("Accuracies std:", np.std(accuracies))
+
+print("Sample")
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+regressor = RandomForestClassifier(n_estimators=100,random_state=0)
+regressor.fit(x_train, y_train)
+y_pred = regressor.predict(x_test)
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
+print(accuracy_score(y_test, y_pred))
+
+
+#considering the scores. 
 dataset_btc['number_comments_s'] = dataset_btc['neg_s'] + dataset_btc['pos_s'] + dataset_btc['neu_s']
 dataset_btc['ratio_pos_s'] = dataset_btc['pos_s']/dataset_btc['number_comments_s']
 dataset_btc['ratio_neg_s'] = dataset_btc['neg_s']/dataset_btc['number_comments_s']
 dataset_btc['ratio_neu_s'] = dataset_btc['neu_s']/dataset_btc['number_comments_s']
 dataset_btc['avg_compound_s'] = ((dataset_btc['comp_s']/dataset_btc['number_comments_s']))**2
-dataset_btc['sq_number_comments_s'] = dataset_btc['number_comments_s']**2
-
+dataset_btc['avg_pos_s'] = (dataset_btc['sum_pos_s']/dataset_btc['pos_s']).replace((np.nan, np.inf, -np.inf), (0, 0, 0))
+dataset_btc['avg_neg_s'] = (dataset_btc['sum_neg_s']/dataset_btc['neg_s']).replace((np.nan, np.inf, -np.inf), (0, 0, 0))
 
 # ax = plt.gca()
 # dataset_positive.plot(kind="scatter",x="number_comments_s", y="ratio_pos_s", color="blue", ax=ax)
@@ -56,19 +70,32 @@ dataset_btc['sq_number_comments_s'] = dataset_btc['number_comments_s']**2
 # plt.show()
 
 #Features
-print(dataset_btc['avg_compound_s'])
-print("mean of the aveage compound_s", dataset_btc['avg_compound_s'].mean())
-print("Variance of the average compound_s", dataset_btc['avg_compound_s'].std())
-x = dataset_btc.iloc[:, [15, 22,26]].values
-y = dataset_btc.iloc[:, 16].values
+# print(dataset_btc['avg_compound_s'])
+# print("mean of the aveage compound_s", dataset_btc['avg_compound_s'].mean())
+# print("Variance of the average compound_s", dataset_btc['avg_compound_s'].var())
+x = dataset_btc.loc[:, ["open","close","high","low","avg_compound_s"]].values
+y = dataset_btc.loc[:, "label"].values
 
+print("With comment scores")
+accuracies = []
+for i in range(100):
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+	regressor = RandomForestClassifier(n_estimators=100)
+	regressor.fit(x_train, y_train)
+
+	y_pred = regressor.predict(x_test)
+	accuracies.append(accuracy_score(y_test, y_pred))
+
+print("Accuracies mean:", np.mean(accuracies))
+print("Accuracies var:", np.var(accuracies))
+print("Accuracies std:", np.std(accuracies))
+
+print("Sample")
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-
-regressor = RandomForestClassifier(n_estimators=40, random_state=0)
+regressor = RandomForestClassifier(n_estimators=100,random_state=0)
 regressor.fit(x_train, y_train)
-
 y_pred = regressor.predict(x_test)
-
 print(confusion_matrix(y_test,y_pred))
 print(classification_report(y_test,y_pred))
 print(accuracy_score(y_test, y_pred))
--- scripts/to_csv.py
+++ scripts/to_csv.py
@@ -48,113 +48,166 @@ submissions_db = reddit_db[SUBMISSIONS_COLLECTION]
 comments_db = reddit_db[COMMENTS_COLLECTION]
 prices_db = market_db[PRICES_COLLECTION]
 
-
+#just subreddits with daily dicussions
 crypto_subreddits = {
-	"BTC":["r\/btc","r\/bitcoin"],
-	"LTC":["r\/litecoin","r\/litecoinmarkets"],
+	"BTC":["r\/bitcoin"],
+	"LTC":["r\/litecoinmarkets"],
 	"XRP":["r\/ripple","r\/xrp"],
-	"ETH":["r\/ethtrader","r\/ethfinance"]
+	"ETH":["r\/ethfinance"]
 }
 
-#open destination file
+regex_daily_discussions = {
+	"r\/bitcoin": "^Daily Discussion,(.*)",
+	"r\/litecoinmarkets": "^\\[Daily Discussion\\](.*)",
+	"r\/ripple": "^Daily Ripple/XRP Discussion Thread(.*)",
+	"r\/xrp": "^Daily XRP Discussion Thread(.*)",
+	"r\/ethfinance": "^Daily General Discussion -(.*)"
+}
+
+def parse_date(title,subreddit):
+	if subreddit == "r\/bitcoin":
+		#format: Daily Discussion, March 13, 2020
+		str_time = title[18:].strip()
+		parse_format = "%B %d, %Y"
+	elif subreddit == "r\/litecoinmarkets":
+		#format: [Daily Discussion] Wednesday, May 08, 2019
+		str_time = title[18:].strip()
+		parse_format = "%A, %B %d, %Y"
+	elif subreddit == "r\/ripple":
+		#format: Daily Ripple/XRP Discussion Thread 04/20/20 [Join Our Discord] invite link: discord.gg/7Bv2rYf
+		str_time = title[35:43]
+		parse_format = "%m/%d/%y"
+	elif subreddit == "r\/xrp":
+		#format: Daily XRP Discussion Thread 01/10/20 [Join Our Discord] invite link: discord.gg/7Bv2rYf
+		str_time = title[28:36]
+		parse_format = "%m/%d/%y"
+	elif subreddit == "r\/ethfinance":
+		#format: Daily General Discussion - February 15, 2020
+		str_time = title.split('-')[1].strip()
+		parse_format = "%B %d, %Y"
+
+	return datetime.strptime(str_time,parse_format).replace(tzinfo=timezone.utc).timestamp()
+
+def init_data(data,time,coin):
+	#add market data
+	market_data = prices_db.find_one({
+		"timestamp": time,
+		"coin": coin
+	})
+
+	op = market_data["open"]
+	close = market_data["close"]
+	high = market_data["high"]
+	low = market_data["low"]
+
+	data[time] = {
+		"time": time,
+		"neg_ns":0,
+		"pos_ns":0,
+		"sum_pos_ns":0,
+		"sum_neg_ns":0,
+		"neutral_ns":0,
+		"comp_ns":0,
+		"neg_s":0,
+		"pos_s":0,
+		"neu_s":0,
+		"sum_pos_s":0,
+		"sum_neg_s":0,
+		"comp_s":0,
+		"coin":coin,
+		"open": op,
+		"close": close,
+		"high": high,
+		"low": low
+	}
+
+def obtain_ids_discussions(subreddit, crypto):
+	s = []
+	regx_title = re.compile(subreddit, re.IGNORECASE)
+	regx_discussion = re.compile(regex_daily_discussions[subreddit])
+	results = submissions_db.find({
+		"subreddit_name_prefixed": regx_title,
+		"title": regx_discussion
+	}).sort([("created_utc",1)])
+	for result in results:
+		s.append((result["title"], "t3_"+result["_id"]))
+	return s
 
 def write_from_db():
 	query_start_date = datetime(2019,9,1).replace(tzinfo=timezone.utc).timestamp()
 	query_end_date =datetime(2020,5,1).replace(tzinfo=timezone.utc).timestamp()
-	file = open('dataset.csv', 'w', newline='')
-	writer = csv.writer(file)
-	writer.writerow(["time", "neg_ns", "pos_ns", "neutral_ns", "comp_ns","neg_s", "pos_s", "neu_s","comp_s","coin","open", "close", "high", "low"])
+	file = open('./datasets/dataset.csv', 'w', newline='')
+	field_names = ["time", "neg_ns", "pos_ns", "neutral_ns","sum_pos_ns","sum_neg_ns",
+	"sum_pos_s","sum_neg_s","comp_ns","neg_s", "pos_s", "neu_s","comp_s","coin","open",
+	"close", "high", "low"]
+	writer = csv.DictWriter(file,field_names)
 	#iterate every cryptocoin over every day in timespan
 	for crypto in crypto_subreddits.keys():
-		for start_time in range(int(query_start_date), int(query_end_date), 86400):
-			negative_comments_ns = 0
-			positive_comments_ns = 0
-			neutral_comments_ns = 0
-			sum_compound_ns = 0
-			negative_comments_s = 0
-			positive_comments_s = 0
-			neutral_comments_s = 0
-			sum_compound_s = 0
+		data = {}
+		for subreddit in crypto_subreddits[crypto]:
+			#find all daily discussions
+			submissions = obtain_ids_discussions(subreddit, crypto)
+			for submission in submissions:
+				title = submission[0]
+				id = submission[1]
+				#parse the datetime of the submission's title
+				time = parse_date(title, subreddit)
 
-			for subreddit_regex in crypto_subreddits[crypto]:
-				#query for all the comments
-				regx = re.compile(subreddit_regex, re.IGNORECASE)
+				#obtain the comments and add them to the data of this day
 				comments = comments_db.find({
-					"created_utc" : {
-										"$gt" : start_time,
-										"$lt": start_time + 86400
-									},
-					"subreddit_name_prefixed": regx
+					"link_id": id
 				})
+
+
+				if time not in data:
+					init_data(data,time,crypto)
+
+				data_row = data[time]
+
 				for comment in comments:
+					if "compound" not in comment:
+						continue
 					compound = comment["compound"]
-					sum_compound_ns += compound
+					data_row["comp_ns"] += compound
 					if compound >= 0.05:
-						positive_comments_ns += 1
+						data_row["pos_ns"] += 1
+						data_row["sum_pos_ns"] += compound
 					elif compound <= -0.05:
-						negative_comments_ns += 1
+						data_row["neg_ns"] += 1
+						data_row["sum_neg_ns"] += compound
 					else:
-						neutral_comments_ns += 1
+						data_row["neutral_ns"] += 1
 
 					#considering the score of the comment:
 					score = comment["score"]
 					if score >= -10:
 						if score > 0:
 							if compound >= 0.05:
-								positive_comments_s += score
+								data_row["pos_s"] += score
+								data_row["sum_pos_s"] += compound*score
 							elif compound <= -0.05:
-								negative_comments_s += score
+								data_row["neg_s"] += score
+								data_row["sum_neg_s"] += compound*score
 							else:
-								neutral_comments_s += score
-							sum_compound_s += compound * score
+								data_row["neu_s"] += score
+							data_row["comp_s"] += compound * score
 
 						if compound >= 0.05:
-							positive_comments_s += 1
+							data_row["pos_s"] += 1
+							data_row["sum_pos_s"] += compound
 						elif compound <= -0.05:
-							negative_comments_s += 1
+							data_row["neg_s"] += 1
+							data_row["sum_neg_ns"] += compound
 						else:
-							neutral_comments_s += 1
-						sum_compound_s += compound
-
-			#add market data
-			market_data = prices_db.find_one({
-				"timestamp": start_time,
-				"coin": crypto
-			})
-
-			op = market_data["open"]
-			close = market_data["close"]
-			high = market_data["high"]
-			low = market_data["low"]
-			
-			writer.writerow([start_time, negative_comments_ns, positive_comments_ns, neutral_comments_ns, sum_compound_ns, negative_comments_s, positive_comments_s, neutral_comments_s, sum_compound_s, crypto,op,close,high,low,number_of_submissions])
+							data_row["neu_s"] += 1
+						data_row["comp_s"] += compound
+		#write rows to csv file
+		for key in data.keys():
+			writer.writerow(data[key])
 	file.close()
 
-def add_number_of_submissions():
-	df = pandas.read_csv('./datasets/dataset.csv')
-	n = len(df)
-	submissions = []
-	for i in range(0,n):
-		crypto = df.iloc[i]['coin']
-		time = int(df.iloc[i]['time'])
-		no_submissions = 0
-		for subreddit_regex in crypto_subreddits[crypto]:
-			regx = re.compile(subreddit_regex, re.IGNORECASE)
-			no_submissions += submissions_db.find({
-				"created_utc" : {
-									"$gt" : time,
-									"$lt": time + 86400
-								},
-				"subreddit_name_prefixed": regx
-			}).count()
-		submissions.append(no_submissions)
-	df['submissions'] = submissions
-	df.to_csv('./datasets/dataset_s.csv')
-
-	print(df)
-
 def add_labels():
-	df = pandas.read_csv('./datasets/dataset_s.csv')
+	df = pandas.read_csv('./datasets/datasets.csv')
 	for crypto in crypto_subreddits.keys():
 		subset = df[df['coin'] == crypto]
 		subset.sort_values(by=['time'], inplace=True)
@@ -170,5 +223,6 @@ def add_labels():
 		
 		filename = "./datasets/" + crypto + ".csv"
 		subset.to_csv(filename, index=False)
+
 
 add_labels()

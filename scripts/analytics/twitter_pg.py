f3b26103075b14bfcfe097536724c4a2bb1d53e1
daniel amezcua <dany_amezcua@live.com.mx>
2020-11-14 19:22:34 -0600

    add close and open price to hourly script

--- scripts/analytics/twitter_pg.py
+++ scripts/analytics/twitter_pg.py
@@ -5,6 +5,10 @@ from scripts.twitter.twitter_mongo import MongoConnectionTweets
 import multiprocessing
 from pymongo import MongoClient
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
 
 def get_tweets_per_hour(tweets):
 	""" used to initialize time series data """
@@ -77,6 +81,55 @@ def get_tweets_reach():
 	end = time.time()
 	print("{} seconds".format(end-start))
 
+def get_historical_data():
+	client = MongoClient(os.environ.get("mongo_url"))
+	db = client["analytics"]
+	collection = db["time_series_1_hour"]
+
+	# just obtain historical data from 2020
+	limit_ts = datetime(2020,1,1,0,0,0,tzinfo=timezone.utc).timestamp()
+	prices_data = defaultdict(dict)
+
+	coins = ["ETH", "BTC", "LTC", "XRP"]
+	for coin in coins:
+		last_ts = datetime.utcnow().replace(minute=0, second=0, microsecond=0, tzinfo=timezone.utc).timestamp()
+		while last_ts > limit_ts:
+			first_ts = True
+			url = "https://min-api.cryptocompare.com/data/v2/histohour?fsym={coin}&tsym=USD&limit=2000&toTs={last_ts}&api_key={api_key}".format(
+				coin = coin,
+				last_ts = last_ts,
+				api_key = os.environ.get("crypto_compare_api_key")
+			)
+			response = requests.get(url)
+			hourly_data = response.json()["Data"]["Data"]
+			for hourly_price in hourly_data:
+				ts = hourly_price["time"] #the first object is the earliest
+				open_price = hourly_price["open"]
+				close_price = hourly_price["close"]
+				prices_data[ts]["{}_open".format(coin.lower())] = open_price
+				prices_data[ts]["{}_close".format(coin.lower())] = close_price
+				if first_ts:
+					last_ts = ts
+					first_ts = False
+
+			print(last_ts)
+
+	for ts,prices in prices_data.items():
+		collection.update_one({"timestamp": ts},{"$set": prices})
+
+def create_time_series(days=1):
+	start = time.time()
+	""" creates a collection of time series of <days> days """
+	client = MongoClient(os.environ.get("mongo_url"))
+	db = client["analytics"]
+	collection = db["time_series_1_hour"]
+	query_options = {"projection": ["created"]}
+	tweets = MongoConnectionTweets.get_documents(**query_options)
+	tweets_per_hour = get_tweets_per_hour(tweets)
+	objects = [{"timestamp" : key, "n_tweets": value} for key,value in tweets_per_hour.items()]
+	collection.insert_many(objects)
+	end = time.time()
+	print("Total time: {} minutes".format((end-start)/60))
 
 def dummy_function(x):
 	print(x)
@@ -84,7 +137,6 @@ def dummy_function(x):
 	for a in x:
 		dummy[a] += 1
 	return dummy
-
 
 def main():
 	start = time.time()
@@ -107,21 +159,6 @@ def main():
 	# plt.plot(list(tweets_per_day.values()))
 	# print(n_tweets)
 	# print(end-start)
-
-def create_time_series(days=1):
-	start = time.time()
-	""" creates a collection of time series of <days> days """
-	client = MongoClient(os.environ.get("mongo_url"))
-	db = client["analytics"]
-	collection = db["time_series_1_hour"]
-	query_options = {"projection": ["created"]}
-	tweets = MongoConnectionTweets.get_documents(**query_options)
-	tweets_per_hour = get_tweets_per_hour(tweets)
-	objects = [{"timestamp" : key, "n_tweets": value} for key,value in tweets_per_hour.items()]
-	collection.insert_many(objects)
-	end = time.time()
-	print("Total time: {} minutes".format((end-start)/60))
-
 
 def time_series_last_hour():
 	""" queries for the tweets of the last hour, builds a time series object and saves it to the database """
@@ -174,6 +211,23 @@ def time_series_last_hour():
 				n_xrp += 1
 				reach_xrp += tweet["followers_user"]
 
+	# get closing and opening price
+	coins = ["ETH", "BTC", "LTC", "XRP"]
+	prices = {}
+
+	for coin in coins:
+		url = "https://min-api.cryptocompare.com/data/v2/histohour?fsym={coin}&tsym=USD&limit=1&api_key={api_key}".format(
+			coin = coin,
+			api_key = os.environ.get("crypto_compare_api_key")
+		)
+		response = requests.get(url)
+		hourly_price = response.json()["Data"]["Data"][1]
+		ts = hourly_price["time"] #the first object is the earliest
+		open_price = hourly_price["open"]
+		close_price = hourly_price["close"]
+		prices["{}_open".format(coin.lower())] = open_price
+		prices["{}_close".format(coin.lower())] = close_price
+
 	time_series_object = {
 		"timestamp" : start_timestamp,
 		"n_tweets": n_tweets,
@@ -187,6 +241,7 @@ def time_series_last_hour():
 		"reach_xrp" : reach_xrp
 	}
 
+	time_series_object.update(prices)
 	print(time_series_object)
 
 	# save data
@@ -194,3 +249,27 @@ def time_series_last_hour():
 	db = client["analytics"]
 	collection = db["time_series_1_hour"]
 	collection.update_one({"timestamp": start_timestamp}, {"$set": time_series_object}, upsert=True)
+
+def visualize_time_series(columns=None):
+	client = MongoClient(os.environ.get("mongo_url"))
+	db = client["analytics"]
+	collection = db["time_series_1_hour"]
+	time_series = collection.find({})
+	fig, ax = plt.subplots()
+	time_series = pd.DataFrame(time_series)
+	
+	min_timestamp = datetime(2020,10,1,0,0,0, tzinfo=timezone.utc).timestamp()
+	max_timestamp = datetime(2020,10,1,23,59,0, tzinfo=timezone.utc).timestamp()
+
+	print(len(time_series))
+	time_series = time_series[time_series["timestamp"] >= min_timestamp]
+	time_series = time_series[time_series["timestamp"] < max_timestamp]
+	print(len(time_series))
+
+	time_series["timestamp"] = time_series["timestamp"].apply(datetime.fromtimestamp)
+
+	ax.plot(time_series["timestamp"], time_series["n_tweets"], 'ro')
+	plt.show()
+
+
+
